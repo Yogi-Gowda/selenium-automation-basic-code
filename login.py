a4d4f3601987d6eec64cf16d5b7e3c8aca79406b
Yogesh K Gowda <yogeshkumar20369@gmail.com>
2026-01-09 16:02:49 +0530

    Update login credentials and URL in login.py

--- login.py
+++ login.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 browser = webdriver.Firefox()
-browser.get('https://www.pesuacademy.com/Academy/')
+browser.get('https://gmail.com')
 #text = browser.find_element(By.ID,value="login_button")
 
 elem = browser.find_element(By.ID,"j_scriptusername")
-elem.send_keys("yogeshkumar20369@gmail.com")
+elem.send_keys("username@gmail.com")
 
 elem = browser.find_element(By.NAME,"j_password")
-elem.send_keys("Yogigowda@4549")
+elem.send_keys("password")
 elem.send_keys(Keys.RETURN)
 time.sleep(5)
